855714d4974c23b17f7694ae0c7bc4616b03dfa3
Naghmeh Farzi <nf1104@jelly.cs.unh.edu>
2025-05-19 16:57:30 -0400

    adding the systm rank as a feature

--- ranklip_command_for_all.sh
+++ ranklip_command_for_all.sh
@@ -2,13 +2,17 @@
 
 BASE_FILTERED_DIR="/home/nf1104/work/Summer 25/LTR_Rubric/train/flant5/dl19/filtered_dl19"
 TRAIN_QREL='/home/nf1104/work/data/dl/data/dl2019/2019binary-qrel.txt'
+ORG_RUN_FILES='/home/nf1104/work/data/dl/data/dl2019/runs_dl2019'
 
 # Shared settings
 OUT_BASE_DIR='ranklips-results/flant5/dl19'
 OUT_PREFIX_CV="cv-5fold"
 EXPERIMENT_NAME="5-fold rank-lips experiment"
 FEAT_PARAM="--feature-variant FeatScore"
-OPT_PARAM="--z-score --default-any-feature-value 0.0 --convergence-threshold 0.0001 --mini-batch-size 100  --folds 5 --restarts 10 --save-heldout-queries-in-model"
+# OPT_PARAM="--z-score --default-any-feature-value 0.0 --convergence-threshold 0.0001 --mini-batch-size 100  --folds 5 --restarts 10 --save-heldout-queries-in-model"
+OPT_PARAM="--z-score --default-any-feature-value -99 --convergence-threshold 0.0001 --mini-batch-size 100  --folds 5 --restarts 10 --save-heldout-queries-in-model"
+
+
 
 for SYSTEM_DIR in "${BASE_FILTERED_DIR}"/*/; do
   SYSTEM_NAME=$(basename "$SYSTEM_DIR")
@@ -19,6 +23,12 @@ for SYSTEM_DIR in "${BASE_FILTERED_DIR}"/*/; do
 
   mkdir -p "${OUT_DIR}"
 
+  #adding each org system run file as a feature temporarily
+  RUN_FILE="${ORG_RUN_FILES}/${SYSTEM_NAME}.run"
+  TMP_RUN_NAME="OrigScore.run"
+  cp "$RUN_FILE" "${TRAIN_FEATURE_DIR}/${TMP_RUN_NAME}"
+  #####
+
   ./rank-lips train --train-cv \
     -d "${TRAIN_FEATURE_DIR}" \
     -q "${TRAIN_QREL}" \
@@ -27,6 +37,7 @@ for SYSTEM_DIR in "${BASE_FILTERED_DIR}"/*/; do
     -O "${OUT_DIR}" \
     -o "${OUT_PREFIX_CV}" \
     ${OPT_PARAM} ${FEAT_PARAM}
+
 
   echo "Predicted Ranking for ${SYSTEM_NAME}:"
   cat "${OUT_DIR}/${OUT_PREFIX_CV}-run-test.run"
@@ -40,4 +51,8 @@ for SYSTEM_DIR in "${BASE_FILTERED_DIR}"/*/; do
     --trec-eval-run \
     -o "${OUT_PREFIX_CV}" \
     ${OPT_PARAM} ${FEAT_PARAM} |& grep -e "Model test test metric" -e "Model train train metric" | tee -a "${OUT_DIR}/MAP_scores.txt"
+
+    ## remove that temp feature
+    rm "${TRAIN_FEATURE_DIR}/${TMP_RUN_NAME}"
+    ###
 done
